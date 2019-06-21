94444bf771999503755f861f76339019ee28fe17
kmadisa <katleho.madisa47@gmail.com>
2019-06-21 20:29:30 +0200

    Running Firefox in headless mode since there is no display.

--- scripts/bet_stats.py
+++ scripts/bet_stats.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 # How to get the account balance.
-driver = webdriver.Firefox()
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.bet.co.za")
 driver.close()
