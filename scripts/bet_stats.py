2a6ba858cee01145e66b3dcbba29e0d9e060103f
kmadisa <katleho.madisa47@gmail.com>
2019-07-23 20:28:28 +0200

    Capture the amount of money place in bets.

--- scripts/bet_stats.py
+++ scripts/bet_stats.py
@@ -6,7 +6,10 @@ from datetime import datetime
 
 from selenium import webdriver 
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.select import Select 
 
+(TICKET, EVENT_DATE, TOURNAMENT, EVENT, SELECTION, BET_TYPE,
+ STAKE, POTENTIAL_WIN, STATUS)  = range(1, 9)
 
 parser = argparse.ArgumentParser(description="Scrape the BET.co.za website"
                                  " to obtain the account balance.")
@@ -16,7 +19,7 @@ parser.add_argument(
     help="Bet.co.za registered email address",)
 parser.add_argument(
     "--password",
-    required=True,
+    required=True, 
     help="Bet.co.za account password.",)
 
 def main():
@@ -41,17 +44,42 @@ def main():
     timestamp = driver.find_element_by_id("time").text.split("Your time: ")
     timestamp = timestamp[-1].strip()
     account_balance = driver.find_element_by_id("blocklogout_userBalanceText").text
-    driver.close()
+    
 
     print("Account Balance: R", account_balance)
     print("Timestamp: ", timestamp)
     date = datetime.date(datetime.now())
     print("Date: ", date)
 
+
+    # Also need to get the amount of funds that is placed in bets
+    driver.find_element_by_link_text('My Betting History').click()
+    # Get the filter form
+    form_filter = driver.find_element_by_id('filter_form')
+    # Create a selector object for dropdown tables
+    selector = Select(form_filter.find_element_by_id('status'))
+    # Select 'Unsettled' bets
+    selector.select_by_visible_text('Unsettled')
+    # Click on the 'Go' button to filter bets
+    form_filter.find_element_by_class_name('inputBtn').click()
+    # Get the table object
+    table = driver.find_element_by_class_name('stdTable')
+    # Get all the rows on column number 7 (Stake)
+    col = table.find_elements_by_xpath("//tr/td["+str(STAKE)+"]")
+
+    # TODO Handle a situation with multiple pages.
+    money_in_bets = 0.00
+    for c in col:
+        money_in_bets += float(c)
+    
+    print("Money in bets: R", money_in_bets)
+
+    driver.close()
+
     with open('balance.csv', mode='w') as balance_file:
         balance_writer = csv.writer(balance_file, delimiter=',',
                                     quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        balance_writer.writerow([date, account_balance, timestamp])
+        balance_writer.writerow([date, account_balance, timestamp, money_in_bets])
 
 if __name__ == "__main__":
     main()
