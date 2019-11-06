d67ca652bbc0cf0854207dc6deb312d295cd55d4
kmadisa <katleho.madisa47@gmail.com>
2019-11-06 20:19:31 +0200

    Removing unused import.

--- mind_your_stonks/web_driver.py
+++ mind_your_stonks/web_driver.py
@@ -4,7 +4,6 @@ import psutil
 
 from loguru import logger
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as condition
