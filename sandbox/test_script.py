7ba454f31fb0ccfcfc7f1464aa80f19317b96ce3
kmadisa <katleho.madisa47@gmail.com>
2019-11-11 20:00:59 +0200

    flake8 compliant changes.

--- sandbox/test_script.py
+++ sandbox/test_script.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from mind_your_stonks.bet_client import BetClient
 
 bc = BetClient("k", "*")
 options = Options()
-options.headless = True  
+options.headless = True
 
 drv = webdriver.Firefox(options=options)
-drv.get('file:///home/kmadisa47/Desktop/ticket3.html') 
+drv.get('file:///home/kmadisa47/Desktop/ticket3.html')
 bc.driver = drv
 
 bet_data = bc._extract_ticket_data()
